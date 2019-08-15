fa39cd983c9b106d0b3e8a2bc561a32fdafe6ee3
Fred Shepherd <fmbshepherd@Freds-MacBook-Pro.local>
2019-08-15 15:14:48 +0200

    stringify

--- src/diff.ts
+++ src/diff.ts
@@ -7,7 +7,7 @@ import * as memoize from 'memoize-one'
 exports.handler = async function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: { ...process.env }
+    body: JSON.stringify({ ...process.env })
   })
   return
   const result = await main(1565787820807, {
